f608b0ded02af7f1b975aee073eb0f0f6d8247a0
Kamal Nadesan <nadesank@grinnell.edu>
2017-03-22 13:11:22 -0500

    Added descriptive notes in the comments Part II

--- server.py
+++ server.py
@@ -53,28 +53,34 @@ def index():
 
 @app.route("/", method=['POST'])
 def search():
+
     # return render('results.html' .... )
     pass
+
 
 @app.route("/new_script", method=['GET'])
 def new_script():
     # return a form for the user to enter a new script
     pass
 
+
 @app.route("/new_script", method=['POST'])
 def save_script():
     # save the script the user uploaded, and return the saved script information
     pass
+
 
 @app.route("/edit_script", method=['GET'])
 def edit_script():
     # return a form with the existing script for editing
     pass
 
+
 @app.route("/update_script", method=['POST'])
 def update_script():
     # take the updated script and save it, and return the script information
     pass
+
 
 @app.route("/show_script", method=['GET'])
 def show_script():
